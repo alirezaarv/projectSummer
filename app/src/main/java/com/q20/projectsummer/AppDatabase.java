dd67238549ef6af6bef964bd48e827b3eafe6a5a
Alireza Arvandi <alireza.arv.mail@gmail.com>
2016-09-10 17:33:19 +0430

    database commit

---
+++ app/src/main/java/com/q20/projectsummer/AppDatabase.java
@@ -0,0 +1,15 @@
+package com.q20.projectsummer;
+
+import android.content.Context;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class AppDatabase extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "data.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+}
