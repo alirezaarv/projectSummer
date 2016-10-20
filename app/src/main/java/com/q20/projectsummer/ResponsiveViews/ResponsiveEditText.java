8bbafec843a8bc7565ca381960f6b6c6021fe32e
IYP-Programer-Yeah <hgsilvermann@yahoo.com>
2016-10-20 17:54:43 +0330

    Fixed responsive text edit

--- app/src/main/java/com/q20/projectsummer/ResponsiveViews/ResponsiveEditText.java
+++ app/src/main/java/com/q20/projectsummer/ResponsiveViews/ResponsiveEditText.java
@@ -2,34 +2,58 @@ package com.q20.projectsummer.ResponsiveViews;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.q20.projectsummer.R;
+
 public class ResponsiveEditText extends EditText implements ResponsiveView{
+
+    private PixelDimensions pixelDimensions;
+    private float polarCenterX=0;
+    private float polarCenterY=0;
+    private float polarRad=0;
+    private float polarTheta=0;
+    private boolean usePolar = false;
+
+
     public ResponsiveEditText(Context context) {
         super(context);
     }
 
     public ResponsiveEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setupPolarCoord(attrs);
     }
 
     public ResponsiveEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setupPolarCoord(attrs);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ResponsiveEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        setupPolarCoord(attrs);
     }
-
-    PixelDimensions pixelDimensions;
 
     public PixelDimensions getPixelDimensions() {
         return pixelDimensions;
+    }
+
+    public void setupPolarCoord(AttributeSet attrs) {
+        TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.ResponsiveEditText, 0, 0);
+        int[] attrsRes= {R.styleable.ResponsiveEditText_PolarCoordCenterX, R.styleable.ResponsiveEditText_PolarCoordCenterY, R.styleable.ResponsiveEditText_PolarCoordRad, R.styleable.ResponsiveEditText_PolarCoordTheta, R.styleable.ResponsiveEditText_UsePolar};
+        polarCenterX = typedArray.getFloat(attrsRes[0], 0);
+        polarCenterY = typedArray.getFloat(attrsRes[1], 0);
+        polarRad = typedArray.getFloat(attrsRes[2], 0);
+        polarTheta = typedArray.getFloat(attrsRes[3], 0);
+        usePolar = typedArray.getBoolean(attrsRes[4], false);
+        typedArray.recycle();
     }
 
     public void calculateDimensions(){
@@ -42,7 +66,7 @@ public class ResponsiveEditText extends EditText implements ResponsiveView{
         float dpX = ScreenDetails.px2Dp(context,params.leftMargin);
         float dpY = ScreenDetails.px2Dp(context,params.topMargin);
 
-        pixelDimensions = new PixelDimensions(dpX,dpY,dpWidth,dpHeight,(View)getParent());
+        pixelDimensions = new PixelDimensions(dpX,dpY,dpWidth,dpHeight,(View)getParent(), polarCenterX, polarCenterY, polarRad, polarTheta, usePolar);
     }
 
     public void updateDimensions(){
