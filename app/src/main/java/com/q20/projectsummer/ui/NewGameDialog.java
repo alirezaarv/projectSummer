09d20cc02e0b5306a7b143d7b764daecb5cce4ba
alireza <alireza.arv.mail@gmail.com>
2016-10-30 16:04:54 +0330

    dialog checking

--- app/src/main/java/com/q20/projectsummer/ui/NewGameDialog.java
+++ app/src/main/java/com/q20/projectsummer/ui/NewGameDialog.java
@@ -1,53 +1,16 @@
 package com.q20.projectsummer.ui;
 
 import android.app.Activity;
-import android.app.DialogFragment;
-import android.content.Context;
+
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 import com.q20.projectsummer.R;
+import com.q20.projectsummer.ResponsiveViews.ScreenDetails;
 
 public class NewGameDialog extends Activity {
-
-//    private Context context;
-//
-//    public void setContext(Context context) {
-//        this.context = context;
-//    }
-//
-//    @Nullable
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View rootView = inflater.inflate(R.layout.fragment_new_game, container, false);
-//
-//        /*((ImageButton) rootView.findViewById(R.id.newGameOnline)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, FindRivalActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        ((ImageButton) rootView.findViewById(R.id.newGameOffline)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, GameActivity.class);
-//                startActivity(intent);
-//            }
-//        });*/
-//
-//        return rootView;
-//    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +21,20 @@ public class NewGameDialog extends Activity {
         this.getWindow().getDecorView().setBackgroundColor(Color.argb(0, 0, 0, 0));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
-        params.height = 1920;
-        params.width = 1080;
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.horizontalMargin = 0;
+        params.verticalMargin = 0;
         this.getWindow().setAttributes(params);
+
+    }
+
+    public void onBackground(View view){
+        finish();
+    }
+
+    public void check(View view){
+
     }
 
     @Override
